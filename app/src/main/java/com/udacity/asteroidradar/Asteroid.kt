246c16f67c59ea06742623aca6b698e5189351a5
bobtal <talevski.boban@gmail.com>
2021-02-14 00:32:14 +0100

    Makes Asteroid a Room entity

--- app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
+++ app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
@@ -1,10 +1,35 @@
 package com.udacity.asteroidradar
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
+@Entity(tableName = "asteroids_table")
 @Parcelize
-data class Asteroid(val id: Long, val codename: String, val closeApproachDate: String,
-                    val absoluteMagnitude: Double, val estimatedDiameter: Double,
-                    val relativeVelocity: Double, val distanceFromEarth: Double,
-                    val isPotentiallyHazardous: Boolean) : Parcelable
+data class Asteroid(
+        @PrimaryKey
+        @ColumnInfo(name = "asteroid_id")
+        val id: Long,
+
+        val codename: String,
+
+        @ColumnInfo(name = "close_approach_date")
+        val closeApproachDate: String,
+
+        @ColumnInfo(name = "absoulute_magnitude")
+        val absoluteMagnitude: Double,
+
+        @ColumnInfo(name = "estimated_diameter")
+        val estimatedDiameter: Double,
+
+        @ColumnInfo(name = "relative_velocity")
+        val relativeVelocity: Double,
+
+        @ColumnInfo(name = "distance_from_earth")
+        val distanceFromEarth: Double,
+
+        @ColumnInfo(name = "is_potentially_hazardous")
+        val isPotentiallyHazardous: Boolean
+        ) : Parcelable
