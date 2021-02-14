fb143aecd1641726b5a4762e9391644d5c589347
bobtal <talevski.boban@gmail.com>
2021-02-14 22:25:23 +0100

    Updates the DAO for the repository needs

--- app/src/main/java/com/udacity/asteroidradar/database/AsteroidDatabaseDao.kt
+++ app/src/main/java/com/udacity/asteroidradar/database/AsteroidDatabaseDao.kt
@@ -1,27 +1,18 @@
 package com.udacity.asteroidradar.database
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 import com.udacity.asteroidradar.Asteroid
 
 @Dao
 interface AsteroidDatabaseDao {
 
-    @Insert
-    fun insertAsteroid(asteroid: Asteroid)
-
-    @Update
-    fun updateAsteroid(asteroid: Asteroid)
-
-    @Query("SELECT * FROM asteroids_table WHERE asteroid_id = :id")
-    fun getAsteroid(id: Long) : Asteroid
-
-    @Query("DELETE FROM asteroids_table")
-    fun clear()
-
     @Query("SELECT * FROM asteroids_table ORDER BY close_approach_date")
     fun getAsteroids() : LiveData<List<Asteroid>>
+
+    @Query("SELECT * FROM asteroids_table")
+    fun getAllAsteroids() : LiveData<List<Asteroid>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg asteroids: Asteroid)
 }
